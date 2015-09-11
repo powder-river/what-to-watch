90d7778e87ed46ad7b257d9edeace9ebe63193d4
Danai Adkisson <dadkisso@live.com>
2015-09-11 13:48:41 -0400

    gets all ratings for a movie if you type in the name as opposed to the id

--- movie.py
+++ movie.py
@@ -2,8 +2,40 @@ import csv
 import re
 
 class Movie:
-    def __init__(self,movie_id):
-        self.movie_id = movie_id
+    def __init__(self,title):
+        self.title = title
+
+    def movie_titles(self):
+        array = []
+        film_titles = []
+        with open('movie_data/u.item.csv', encoding='latin_1') as f:
+            reader = csv.reader(f,delimiter='|')
+            # reader = csv.DictReader(f, fieldnames=['title', 'movie_title', '', '', 'something_else'], delimiter='|')
+            for row in reader:
+                array.append([row[0],row[1]])
+
+        for n in array:
+            n[1] = re.sub(r'\(\d+\)', "", n[1])
+            film_titles.append(n)
+        return film_titles
+
+
+    def get_movie_id(self, title_list):
+        for n in title_list:
+            if self.title in n[1].lower():
+                return n[0]
+
+
+    def get_movie_name(self,title_list):
+        for n in title_list:
+            if self.title in n[1].lower():
+                return n[1]
+
+    # def movie_id(self):
+    #     with open('u.item', encoding='latin_1') as f:
+    #         reader = csv.DictReader(f, fieldnames=['movie_id', 'movie_title', '', '', 'something_else'], delimiter='|')
+    #     for row in reader:
+    #         print(row)
 
     def user_rating_list(self):
         array = []
@@ -28,9 +60,11 @@ class Movie:
         return round(average,1)
 
 
-    def movie_ratings(self, database):
-        for i in database:
-            if i[]
+    def movie_ratings(self, user_ratings, movie_id):
+
+        for i in user_ratings:
+            if i[1] == movie_id :
+                print("User ID: {},  Rating: {}".format(i[0], i[2]))
 
 
 
--- movie_lib.py
+++ movie_lib.py
@@ -10,16 +10,30 @@ from user import *
 [x]Find all ratings for a user
 
 """
+def title_to_id(title):
+    title
+
 def user_ratings():
     user_id = input("Enter a user id to find that user's reviews\n")
     user = User(user_id)
     review_list = user.user_rating_list()
     print("Here all all movie reviews of user id: {}".format(user_id))
-    user_reviews = user.movie_ratings(review_list)
+    user.movie_ratings(review_list)
     # print("Here are all movies reviewd by user {}: {}".format(user_id, user_reviews))
 
 
 def movie_ratings():
+    movie_title = input("Enter Movie Title\n").lower()
+    movie = Movie(movie_title)
+    titles = movie.movie_titles()
+    movie_id = movie.get_movie_id(titles)
+
+    print("Getting Results For: {}".format(movie.get_movie_name(titles)))
+    movie = Movie(movie_id)
+    review_list = movie.user_rating_list()
+
+    movie.movie_ratings(review_list, movie_id)
+
 
 def average_rating():
     movie_id = input("Enter Movie ID\n")
@@ -36,12 +50,15 @@ def main():
 
     [1]-- Find The Average Review of a Movie
     [2]-- Find All Reviews From a User
+    [3]-- Find All Reviews For a Movie
     """)
 
     if choice == '1':
         average_rating()
     elif choice == '2':
         user_ratings()
+    elif choice == '3':
+        movie_ratings()
     else:
         print("Goodbye")
 
